88925c506b6d8b5330ed7279af0f595602255cb0
dhiamonkey <dhiamuhammad@gmail.com>
2021-05-17 22:40:39 +0700

    stupid doesnt work

--- pages/aspiring-content.js
+++ pages/aspiring-content.js
@@ -353,27 +353,12 @@ export default function AspiringContent() {
 
     </div>
 
+    </div>
 
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-    <script src="js/jqueryInclude.js"></script>
-    <script src="js/smoothscroll.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/animsition.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script src="js/jquery.pagepiling.min.js"></script>
-    <script src="js/isotope.pkgd.min.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
-    <script src="js/TweenMax.min.js"></script>
-    <script src="js/ScrollMagic.min.js"></script>
-    <script src="js/animation.gsap.min.js"></script>
-    <script src="js/jquery.viewport.js"></script>
-    <script src="js/jquery.countdown.min.js"></script>
-    <script src="js/script.js"></script>
+    
 
 
-  </div>
+  
 
 </>
 
