33561cfee7455e287fe807f9eec3b83bb28b4de6
dhiamonkey <dhiamuhammad@gmail.com>
2021-05-25 15:06:25 +0700

    start anew

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     
 
     <title>Dhiamuhammad - Portfolio</title>
-
+    
     {/* <!-- CSS --> */}
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/animsition.min.css" rel="stylesheet" />
@@ -30,6 +30,7 @@ export default function Home() {
     <link rel="manifest" href="favicon/site.webmanifest" />
     <meta name="msapplication-TileColor" content="#da532c" />
     <meta name="theme-color" content="#ffffff" />
+    
     
   </Head>
 
@@ -319,7 +320,24 @@ export default function Home() {
     </div>
     
     </div>
-    
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="js/jqueryInclude.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.pagepiling.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/TweenMax.min.js"></script>
+    <script src="js/ScrollMagic.min.js"></script>
+    <script src="js/animation.gsap.min.js"></script>
+    <script src="js/jquery.viewport.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/custom.js"></script>
     
 
 
